1e678cb17399074d208f29537cddc31ce8a0834d
GURUMANCHI-SAI-KALYAN <kalyangurumanchi@gmail.com>
2021-01-15 19:41:52 +0530

    ADDED: CREATING TICTACTOE BOARD ASSIGNING EMPTY PLACES PROGRAM

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,5 +1,15 @@
-public class TicTacToe {
-	public static void main(String[] args) {
-		System.out.println("welcome to tic tac toe game");
-	}
+package com.workshop;
+
+public class Tictactoe {
+
+    public static void main(String[] args) {
+        char[] board=createBoard();
+    }
+    public static char[] createBoard(){
+        char[] board=new char[10];
+        for (int i=0;i< board.length;i++){
+            board[i]=' ';
+        }
+        return board;
+    }
 }
