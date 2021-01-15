38c6c4a979d49ef56cf38a5feafafcb5701a7399
GURUMANCHI-SAI-KALYAN <kalyangurumanchi@gmail.com>
2021-01-15 22:27:03 +0530

    ADDED : SELECTING THE SYMBOL USER WANT UC2 PROGRAM

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,10 +1,16 @@
-package com.workshop;
+import java.util.Scanner;
 
-public class Tictactoe {
+public class TicTacToe {
+	public char player_Symbol;
+	public char computer_Symbol;
 
     public static void main(String[] args) {
         char[] board=createBoard();
-    }
+	TicTacToe tictactoe = new TicTacToe();
+        Scanner userInput = new Scanner(System.in);
+	System.out.println("choose your letter: ");
+	tictactoe.chooseUserLetter(userInput.next());
+}
     public static char[] createBoard(){
         char[] board=new char[10];
         for (int i=0;i< board.length;i++){
@@ -12,4 +18,15 @@ public class Tictactoe {
         }
         return board;
     }
+
+    public void chooseUserLetter(String symbol) {
+        if (symbol.equals("X")) {
+		player_Symbol = 'X';
+		computer_Symbol = '0';
+	}
+	else if (symbol.equals("0")) {
+		player_Symbol = '0';
+		computer_Symbol = 'X';
+	}
+   }
 }
